9f9f9fe1596a7ca627a43dc81b0de36a1834f763
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-05-15 22:29:19 +0300

    фоновая 6.1 со звуком

--- Простые формы/Фоновая 6.1/Form1.Designer.cs
+++ Простые формы/Фоновая 6.1/Form1.Designer.cs
@@ -57,11 +57,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 160);
+            this.ClientSize = new System.Drawing.Size(482, 153);
             this.Controls.Add(this.B);
             this.Controls.Add(this.A);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 200);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Веселая форма";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
